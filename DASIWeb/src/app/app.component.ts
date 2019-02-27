2220e5306b076d27967c18d204bcbb682d53b3f1
hugoshiroma <hugo.shiroma@gmail.com>
2019-02-26 23:16:24 -0300

    troquei umas cor aí

--- DASIWeb/src/app/app.component.ts
+++ DASIWeb/src/app/app.component.ts
@@ -16,13 +16,13 @@ export class AppComponent implements OnInit {
   delay = timer(5000);
 
   @HostListener('window:scroll', ['$event']) onScroll(event: any) {
-      if (event.srcElement.scrollingElement.scrollTop > document.body.offsetHeight - 115) {
+      if (event.srcElement.scrollingElement.scrollTop > document.body.offsetHeight - 100) {
           this.changeHeader = true;
       } else {
           this.changeHeader = false;
       }
 
-      if (event.srcElement.scrollingElement.scrollTop > document.body.offsetHeight - 115) {
+      if (event.srcElement.scrollingElement.scrollTop > document.body.offsetHeight - 100) {
           this.fixSubheader = true;
       } else {
           this.fixSubheader = false;
